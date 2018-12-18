26cf1a8f8d00494aa9870f6527eb50f6b53d9705
jsa-aerial <jsa.aerial@gmail.com>
2018-12-17 21:02:20 -0500

    Bump to 0.2.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aerial.hanasu "0.2.2"
+(defproject aerial.hanasu "0.2.3"
   :description "Light weight simple websocket communications"
   :url "https://github.com/jsa-aerial/hanasu"
   :license {:name "The MIT License (MIT)"
